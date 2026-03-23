b3ed181fb1411a3bc60a408d4972f13432503ef8
Sofiia Maksymchuk <sofiiamaksymchuk@Sofiias-MacBook-Air.local>
2026-03-23 15:11:00 -0400

    add separate vite base for github pages and netlify

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/",
-});
+export default defineConfig(({ mode }) => ({
+  base: mode === "github" ? "/art-site/" : "/",
+}));
